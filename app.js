40094caf0a95e9b9c3047194e69a4f835e491fb6
Gustavo Rodrigues <gustardn@gmail.com>
2023-06-07 16:58:24 -0300

    added iframe player api to have more access of the iframe

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let initialScroll = 0;
-let nav = document.getElementsByClassName("navbar")[0];
 
-document.addEventListener("scroll", (event) => {
+function setNavbar(event) {
+  let nav = document.getElementsByClassName("navbar")[0];
   let newScroll = event.target.documentElement.scrollTop;
+
   if (newScroll === 0) {
     nav.classList.remove("navbar-background");
   } else if (initialScroll < newScroll) {
@@ -12,4 +13,38 @@ document.addEventListener("scroll", (event) => {
     nav.classList.add("navbar-background");
   }
   initialScroll = newScroll;
+}
+
+document.addEventListener("scroll", (event) => {
+  setNavbar(event);
 });
+
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "100%",
+    width: "100%",
+    videoId: "VJH5FJ5kaJA",
+    playerVars: {
+      playsinline: 1,
+      mute: 1,
+      controls: 0,
+      disablekb: 1,
+      loop: 1,
+    },
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  console.log(event);
+  event.target.playVideo();
+}
